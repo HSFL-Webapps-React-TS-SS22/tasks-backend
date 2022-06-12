0d4f9d4fdcec7f303b4f5c355ed20f4d3b772b7c
Boris Dudelsack <bdudelsack@gmail.com>
2022-06-12 14:12:36 +0200

    Update swagger documentation

--- src/main/kotlin/de/progeek/tasks/config/OpenApiConfiguration.kt
+++ src/main/kotlin/de/progeek/tasks/config/OpenApiConfiguration.kt
@@ -8,12 +8,19 @@ import org.springframework.http.HttpMethod
 import springfox.documentation.builders.*
 import springfox.documentation.schema.AlternateTypeRules
 import springfox.documentation.schema.WildcardType
+import springfox.documentation.service.BasicAuth
+import springfox.documentation.service.HttpAuthenticationScheme
+import springfox.documentation.service.SecurityReference
+import springfox.documentation.service.SecurityScheme
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 
 
 @Configuration
 class OpenApiConfiguration(val typeResolver: TypeResolver) {
+    val BASIC_AUTH_NAME = "basic"
+
     @Bean
     fun treenetalerApi(): Docket {
 
@@ -22,7 +29,6 @@ class OpenApiConfiguration(val typeResolver: TypeResolver) {
             ResponseBuilder().code("403").description("Forbidden").build(),
             ResponseBuilder().code("500").description("Internal Server Error").build()
         )
-
         return Docket(DocumentationType.OAS_30)
             .apiInfo(apiInfo())
             .useDefaultResponseMessages(false)
@@ -36,10 +42,27 @@ class OpenApiConfiguration(val typeResolver: TypeResolver) {
                     typeResolver.resolve(List::class.java, WildcardType::class.java)
                 )
             )
+            .securitySchemes(arrayListOf(basicAuth()))
+            .securityContexts(arrayListOf(basicSecurityContext()))
             .select()
             .paths(PathSelectors.any())
             .build()
+
     }
+
+
+    private fun basicSecurityContext(): SecurityContext {
+        return SecurityContext.builder()
+            .securityReferences(listOf(SecurityReference(BASIC_AUTH_NAME, arrayOf())))
+            .forPaths(PathSelectors.ant("/task/**"))
+            .build()
+    }
+
+    private fun basicAuth(): SecurityScheme = HttpAuthenticationScheme.BASIC_AUTH_BUILDER
+        .name(BASIC_AUTH_NAME)
+        .description("Basic authentication")
+        .build()
+
 
     private fun apiInfo() = ApiInfoBuilder()
         .title("Tasks Management API")
--- src/main/kotlin/de/progeek/tasks/config/SecurityConfiguration.kt
+++ src/main/kotlin/de/progeek/tasks/config/SecurityConfiguration.kt
@@ -2,12 +2,16 @@ package de.progeek.tasks.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity.http
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebFluxSecurity
@@ -23,12 +27,30 @@ class SecurityConfiguration {
         return http
             .csrf().disable()
             .authorizeExchange()
-            .pathMatchers("/user").permitAll()
-            .anyExchange().authenticated()
+            .pathMatchers("/task").authenticated()
+            .pathMatchers("/**").permitAll()
+            .and()
+            .cors().configurationSource(createCorsConfigSource())
             .and()
             .httpBasic()
             .and()
             .formLogin().disable()
             .build();
+    }
+
+    fun createCorsConfigSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+        corsConfig.applyPermitDefaultValues()
+        corsConfig.allowedOriginPatterns = listOf("*")
+        corsConfig.addAllowedMethod(HttpMethod.GET)
+        corsConfig.addAllowedMethod(HttpMethod.OPTIONS)
+        corsConfig.addAllowedMethod(HttpMethod.DELETE)
+        corsConfig.maxAge = 8000L
+        corsConfig.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+
+        return source
     }
 }
